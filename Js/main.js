dae6df4f8dfa8c8cdd3228fda6658fa44480ccb6
Masud Rana <119274830+developermrana@users.noreply.github.com>
2026-01-09 01:01:19 +0600

    Add files via upload
    full website upload

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+(() => {
+  const forms = document.querySelectorAll("#contactForm");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
