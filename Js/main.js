613a2facb2fc22d109dca4ecb2a2bfcc4970a591
Masud Rana <119274830+developermrana@users.noreply.github.com>
2026-01-09 01:25:08 +0600

    Update main.js
    add submit alert

--- Js/main.js
+++ Js/main.js
@@ -1,18 +1,17 @@
-(() => {
-  const forms = document.querySelectorAll("#contactForm");
+const forms = document.querySelectorAll("#contactForm");
 
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
+forms.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+      form.classList.add("was-validated");
+    } else {
+      event.preventDefault();
+      form.classList.add("was-validated");
+      alert("Form Submitted Successfully");
+      form.reset();
+      form.classList.remove("was-validated");
+    }
   });
-})();
+});
